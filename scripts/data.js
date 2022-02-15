9d1e1a9f34fada5d6720b9ee3bd339fedf9d7d5b
Razia <nrwpc.stack@gmail.com>
2022-02-15 23:48:07 +0800

    Census Total Population Structure
    initial working set of total population structure after data transformation

--- scripts/data.js
+++ scripts/data.js
@@ -1,40 +1,74 @@
 const DOS_TABLE_API = {
-    BASE: 'https://tablebuilder.singstat.gov.sg/api/table/tabledata/',
+    BASE_URL: 'https://tablebuilder.singstat.gov.sg/api/table/tabledata/',
+    STORE_URL: 'assets/data/temp/',
     POPULATION_STRUCTURE: {
-        2020: '17394',
-        2010: '8537',
-        2000: '8850'
+        2020: 17394,
+        2010: 8537,
+        2000: 8850
     }
 };
 
-async function getCensusData(tableId) {
-    let data = {};
+async function getAllCensusData() {
+    let promiseArr = [];
+    let storedPromiseArr = [];
+    let censusData = {};
 
-    try {
-        let resp = await axios.get(DOS_TABLE_API.BASE + tableId);
-        data = resp.data;
-    } catch (e) {
-        console.log('Unable to get data of DOS census via singstag.gov.sg, attempting to use stored copy of', tableId);
-    } finally {
-        data = getStoredCensusData(tableId);
+    for (let prop in DOS_TABLE_API.POPULATION_STRUCTURE) {
+        promiseArr.push(axios.get(DOS_TABLE_API.BASE_URL + DOS_TABLE_API.POPULATION_STRUCTURE[prop]));
+        storedPromiseArr.push(axios.get(DOS_TABLE_API.STORE_URL + DOS_TABLE_API.POPULATION_STRUCTURE[prop] + '.json'))
     }
 
-    return data;
-}
+    await Promise.all(promiseArr).then(resp => {
+        for (let r of resp) {
+            if (Object.values(DOS_TABLE_API.POPULATION_STRUCTURE).includes(r.data.ID)) {
+                censusData[util.getKeyByValue(DOS_TABLE_API.POPULATION_STRUCTURE, r.data.ID)] = r.data.row;
+            }
+        }
+    }).catch(async error => {
+        console.log('Unable to get data of DOS census via singstag.gov.sg, attempting to use stored copies');
+        await Promise.all(storedPromiseArr).then(resp => {
+            for (let r of resp) {
+                if (Object.values(DOS_TABLE_API.POPULATION_STRUCTURE).includes(r.data.ID)) {
+                    censusData[util.getKeyByValue(DOS_TABLE_API.POPULATION_STRUCTURE, r.data.ID)] = r.data.row;
+                }
+            }
+        }).catch(error => {
+            console.log('Unable to get data of DOS census stored copies');
+        });
+    });
 
-async function getStoredCensusData(tableId) {
-    let data = {};
-
-    try {
-        let resp = await axios.get('assets/data/temp/' + tableId + '.json');
-        data = resp.data;
-    } catch (e) {
-        console.error('Issue with DOS census stored copy', e);
-    }
-
-    return data;
+    console.log("Census Data Response:", censusData);
+    return censusData;
 }
 
 function transformedCensusData(rawData) {
+    let totalStructure = {};
+    // let ageGroup = {};
+    // let genderGroup = {};
+    // let raceGroup = {};
+    // let residency = {};
 
+    for (let key in rawData) {
+        totalStructure[key] = {
+            total: Number(rawData[key][0].columns[0].columns[0].columns[0].Value),
+            gender: {
+                male: Number(rawData[key][0].columns[0].columns[0].columns[1].Value),
+                female: Number(rawData[key][0].columns[0].columns[0].columns[2].Value)
+            },
+            race: {
+                chinese: Number(rawData[key][0].columns[0].columns[1].columns[0].Value),
+                malay: Number(rawData[key][0].columns[0].columns[2].columns[0].Value),
+                indian: Number(rawData[key][0].columns[0].columns[3].columns[0].Value),
+                others: Number(rawData[key][0].columns[0].columns[4].columns[0].Value)
+            },
+            residency: {
+                citizen: Number(rawData[key][0].columns[1].columns[0].columns[0].Value),
+                permanent: Number(rawData[key][0].columns[2].columns[0].columns[0].Value)
+            }
+
+        };
+    }
+
+    console.log("Total Population Structure:", totalStructure);
+    return totalStructure;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,77 @@
 const MAP_ZOOM_INITIAL = 11;
 
 function main() {
-    let gd_census = [];
 
     function init() {
         initHeader();
 
         window.addEventListener('DOMContentLoaded', async function() {
+            let censusSeries = await initCensus();
+            let censusYearsArr = Object.keys(censusSeries);
 
+            console.log(censusSeries);
+
+            const raceOpt = {
+                chart: {
+                    type: 'pie',
+                    height: "100%"
+                },
+                series: [censusSeries['2020'].race.chinese, censusSeries['2020'].race.malay, censusSeries['2020'].race.indian, censusSeries['2020'].race.others],
+                labels: ['Chinese', 'Malay', 'Indian', 'Others']
+            }
+            const racePie = new ApexCharts(document.getElementById('pie-race'), raceOpt);
+            racePie.render();
+
+            const genderOpt = {
+                chart: {
+                    type: 'pie',
+                    height: "100%"
+                },
+                series: [censusSeries['2020'].gender.male, censusSeries['2020'].gender.female],
+                labels: ['Male', 'Female']
+            }
+            const genderPie = new ApexCharts(document.getElementById('pie-gender'), genderOpt);
+            genderPie.render();
+
+            const resOpt = {
+                chart: {
+                    type: 'pie',
+                    height: "100%"
+                },
+                series: [censusSeries['2020'].residency.citizen, censusSeries['2020'].residency.permanent],
+                labels: ['Citizen', 'Permanent Resident']
+            }
+            const resPie = new ApexCharts(document.getElementById('pie-residency'), resOpt);
+            resPie.render();
+
+            const totalOpt = {
+                chart: {
+                    type: 'line',
+                    height: "100%"
+                },
+                series: [{
+                        name: 'Chinese',
+                        data: censusYearsArr.map(k => censusSeries[k].race.chinese)
+                    },
+                    {
+                        name: 'Malay',
+                        data: censusYearsArr.map(k => censusSeries[k].race.malay)
+                    },
+                    {
+                        name: 'Indian',
+                        data: censusYearsArr.map(k => censusSeries[k].race.indian)
+                    },
+                    {
+                        name: 'Others',
+                        data: censusYearsArr.map(k => censusSeries[k].race.others)
+                    }
+                ],
+                xaxis: {
+                    categories: censusYearsArr
+                }
+            }
+            const chart = new ApexCharts(document.getElementById('total'), totalOpt);
+            chart.render();
         });
     }
 
@@ -21,8 +85,9 @@ function main() {
         });
     }
 
-    function initCensus() {
-        gd_census = getCensusData();
+    async function initCensus() {
+        let rawData = await getAllCensusData();
+        return transformedCensusData(rawData);
     }
 
     init();
