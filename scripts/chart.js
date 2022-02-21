7b20f49a54c761cfc532068b0818fcac0b14a1ed
Razia <nrwpc.stack@gmail.com>
2022-02-21 16:33:32 +0800

    Area graphs of residency type
    - create synchronised area graphs for citizens, pr, and non-residency
    - added downloaded JSON due to issue with returned API

--- scripts/chart.js
+++ scripts/chart.js
@@ -25,94 +25,127 @@ function displayPieChart(type, elementId, seriesArray, labelArray) {
     pie.render();
 }
 
-function displayAnnualPopulationChart(populationData) {
-    let years = populationData.ascYear.slice(-30);
-    let yearData = populationData.dataByYear;
-
-    //displayPieChart('donut', 'population-res', [yearData[latestYear][LABELS.SG_PPLT], yearData[latestYear][LABELS.PR_PPLT], yearData[latestYear][LABELS.NON_RES_PPLT]], [LABELS.SG_PPLT, LABELS.PR_PPLT, LABELS.NON_RES_PPLT]);
-
-    var options = {
-        series: [{
-            name: LABELS.CITIZEN,
-            data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.CITIZEN_PPLT]) })
-        }, {
-            name: LABELS.PR,
-            data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.PR_PPLT]) })
-        }, {
-            name: LABELS.NON_RES,
-            data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.PR_PPLT]) })
-        }],
+function displayResidencySparkLines(id, name, labels, seriesData, subtitle) {
+    let options = {
         chart: {
-            type: 'bar',
-            height: 700,
-            stacked: true,
-            zoom: {
+            id: id,
+            group: 'residency',
+            type: 'area',
+            height: 160,
+            sparkline: {
                 enabled: true
-            }
+            },
         },
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                legend: {
-                    position: 'bottom',
-                    offsetX: -10,
-                    offsetY: 0
-                }
-            }
-        }],
-        plotOptions: {
-            bar: {
-                horizontal: true
-            }
-        },
-        xaxis: {
-            categories: years
-        },
-        legend: {
-            position: 'top'
+        stroke: {
+            curve: 'smooth'
         },
         fill: {
-            opacity: 1
+            opacity: 1,
+        },
+        series: [{
+            name: name,
+            data: seriesData
+        }],
+        labels: labels,
+        yaxis: {
+            min: 0
+        },
+        title: {
+            text: name,
+            offsetX: 30,
+            style: {
+                fontSize: '24px',
+                cssClass: 'apexcharts-yaxis-title'
+            }
+        },
+        subtitle: {
+            text: subtitle,
+            offsetX: 30,
+            style: {
+                fontSize: '14px',
+                cssClass: 'apexcharts-yaxis-title'
+            }
         }
     };
 
-    var chart = new ApexCharts(document.getElementById("population-stack"), options);
+    let chart = new ApexCharts(document.getElementById(id), options);
     chart.render();
 }
 
-function displayCensusCharts(censusSeries) {
-    let censusYearsArr = Object.keys(censusSeries);
+function renderAnnualPopulationChart(populationData) {
+    let years = populationData.ascYear.slice(-30);
+    let yearData = populationData.dataByYear;
+    let latestData = yearData[years[years.length - 1]];
 
-    displayPieChart('pie', 'pie-race', [censusSeries['2020'].race.chinese, censusSeries['2020'].race.malay, censusSeries['2020'].race.indian, censusSeries['2020'].race.others], ['Chinese', 'Malay', 'Indian', 'Others']);
-    displayPieChart('pie', 'pie-gender', [censusSeries['2020'].gender.male, censusSeries['2020'].gender.female], ['Male', 'Female']);
-    displayPieChart('pie', 'pie-residency', [censusSeries['2020'].residency.citizen, censusSeries['2020'].residency.permanent], ['Citizen', 'Permanent Resident']);
+    displayResidencySparkLines(
+        'citizen-trend',
+        CHART_LABELS.CITIZEN,
+        years,
+        years.map(y => { return yearData[y][DOS_DATA_KEYS.CITIZEN_PPLT] }),
+        'Latest number: ' + latestData[DOS_DATA_KEYS.CITIZEN_PPLT]
+    );
 
-    const totalOpt = {
-        chart: {
-            type: 'line',
-            height: "100%"
-        },
-        series: [{
-                name: 'Chinese',
-                data: censusYearsArr.map(k => censusSeries[k].race.chinese)
-            },
-            {
-                name: 'Malay',
-                data: censusYearsArr.map(k => censusSeries[k].race.malay)
-            },
-            {
-                name: 'Indian',
-                data: censusYearsArr.map(k => censusSeries[k].race.indian)
-            },
-            {
-                name: 'Others',
-                data: censusYearsArr.map(k => censusSeries[k].race.others)
-            }
-        ],
-        xaxis: {
-            categories: censusYearsArr
-        }
-    }
-    const chart = new ApexCharts(document.getElementById('total'), totalOpt);
-    chart.render();
+    displayResidencySparkLines(
+        'pr-trend',
+        CHART_LABELS.PR,
+        years,
+        years.map(y => { return yearData[y][DOS_DATA_KEYS.PR_PPLT] }),
+        'Latest number: ' + latestData[DOS_DATA_KEYS.PR_PPLT]
+    );
+
+    displayResidencySparkLines(
+        'nonres-trend',
+        CHART_LABELS.NON_RES,
+        years,
+        years.map(y => { return yearData[y][DOS_DATA_KEYS.NON_RES_PPLT] }),
+        'Latest number: ' + latestData[DOS_DATA_KEYS.NON_RES_PPLT]
+    );
+
+    // var options = {
+    //     series: [{
+    //         name: LABELS.CITIZEN,
+    //         data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.CITIZEN_PPLT]) })
+    //     }, {
+    //         name: LABELS.PR,
+    //         data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.PR_PPLT]) })
+    //     }, {
+    //         name: LABELS.NON_RES,
+    //         data: years.map(y => { return UTIL.convertToNumber(yearData[y][LABELS.PR_PPLT]) })
+    //     }],
+    //     chart: {
+    //         type: 'bar',
+    //         height: 700,
+    //         stacked: true,
+    //         zoom: {
+    //             enabled: true
+    //         }
+    //     },
+    //     responsive: [{
+    //         breakpoint: 480,
+    //         options: {
+    //             legend: {
+    //                 position: 'bottom',
+    //                 offsetX: -10,
+    //                 offsetY: 0
+    //             }
+    //         }
+    //     }],
+    //     plotOptions: {
+    //         bar: {
+    //             horizontal: true
+    //         }
+    //     },
+    //     xaxis: {
+    //         categories: years
+    //     },
+    //     legend: {
+    //         position: 'top'
+    //     },
+    //     fill: {
+    //         opacity: 1
+    //     }
+    // };
+
+    // var chart = new ApexCharts(document.getElementById("population-stack"), options);
+    // chart.render();
 }
--- scripts/util.js
+++ scripts/util.js
@@ -6,10 +6,12 @@ const UTIL = {
     convertToNumber: (value) => isNaN(value) ? 0 : Number(value),
     getKeyByValue: (object, value) => Object.keys(object).find(key => object[key] === value)
 }
-const LABELS = {
+const CHART_LABELS = {
     CITIZEN: 'Singapore Citizen',
     PR: 'Permanent Resident',
-    NON_RES: 'Non-Resident',
+    NON_RES: 'Non-Resident'
+}
+const DOS_DATA_KEYS = {
     CITIZEN_PPLT: 'Singapore Citizen Population',
     PR_PPLT: 'Permanent Resident Population',
     NON_RES_PPLT: 'Non-Resident Population'
