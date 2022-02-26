46e2069143bc6ddf8e4be6b7d69f24bdc8e177f1
Razia <nrwpc.stack@gmail.com>
2022-02-26 19:56:39 +0800

    Refactor data.js
    - refactor code for data transformation of each DOS table
    - replace Object.assign with spread

--- scripts/data.js
+++ scripts/data.js
@@ -27,192 +27,48 @@ async function getGeoDistributionData() {
         });
     });
 
-    console.log("Geographical Distribution Response:\n", data);
+    // console.log("Geographical Distribution Response:\n", data);
     return data;
 }
 
+function getObjModelForGeoDistribution(rawDataByCat, isAgeGroup) {
+    let dataByCat = {};
+    for (let rowObj of rawDataByCat) {
+        let areaName = UTIL.convertToTitleCase(rowObj['rowText'].toLowerCase().replace('- total', '').trim());
+
+
+        for (let colData of rowObj.columns) {
+            let dataKey = colData['key'];
+            let dataToAdd = {};
+
+            if (isAgeGroup) {
+                for (let data of colData.columns) {
+                    let { key, value } = data;
+                    dataToAdd[dataKey] = {...dataToAdd[dataKey], [key]: UTIL.convertToNumber(value) };
+                }
+            } else {
+                if (colData.hasOwnProperty('value')) {
+                    value = UTIL.convertToNumber(colData['value']);
+                } else if (colData.hasOwnProperty('columns')) {
+                    let obj = colData['columns'].find(i => i['key'].toLowerCase() == 'total');
+                    value = obj ? UTIL.convertToNumber(obj['value']) : colData['columns'];
+                }
+
+                dataToAdd = {
+                    [dataKey]: value
+                };
+            }
+
+            dataByCat[areaName] = {...dataByCat[areaName], ...dataToAdd };
+        }
+    }
+
+    return dataByCat;
+}
+
 function transformGeoDistributionData(rawData) {
-    let dataByArea = {
-        ageGroup: {},
-        ageGroupGender: {},
-        ethnicGroup: {},
-        dwellingType: {},
-        grossIncome: {},
-        literacy: {},
-        occupation: {},
-        qualification: {},
-        transportMode: {},
-        highestPopulationCount: 0
-    };
-
-    for (let rowObj of rawData.dwellingType) {
-        let areaName = rowObj['rowText'];
-
-        for (let dwellingObj of rowObj.columns) {
-            let dataKey = dwellingObj['key'];
-            let value = dwellingObj.hasOwnProperty('value') ? dwellingObj['value'] : (dwellingObj['columns'].find(hdb => hdb['key'].toLowerCase() === 'total'))['value'];
-            value = UTIL.convertToNumber(value);
-            if (dataKey.toLowerCase() !== 'total') {
-                if (dataByArea.dwellingType.hasOwnProperty(areaName)) {
-                    Object.assign(dataByArea.dwellingType[areaName], {
-                        [dataKey]: value
-                    });
-                } else {
-                    dataByArea.dwellingType[areaName] = {
-                        [dataKey]: value
-                    };
-                }
-            }
-        }
-    }
-
+    let dataByArea = { highestPopulationCount: 0 };
     let ageGroupByArea = rawData.ageGroup.filter(d => d['rowText'].toLowerCase().includes('total'));
-    for (let rowObj of ageGroupByArea) {
-        let areaName = UTIL.convertToTitleCase(rowObj['rowText'].toLowerCase().replace('- total', '').trim());
-
-        for (let ageCol of rowObj.columns) {
-            let genderKey = ageCol['key'];
-
-            for (let ageGroupObj of ageCol.columns) {
-                let dataKey = ageGroupObj['key'];
-                let value = UTIL.convertToNumber(ageGroupObj['value']);
-
-                if (areaName.toLowerCase() !== 'total' && dataKey.toLowerCase() == 'total' && value > dataByArea.highestPopulationCount) {
-                    dataByArea.highestPopulationCount = value;
-                }
-
-                if (genderKey.toLowerCase() == 'total') {
-                    if (dataByArea.ageGroup.hasOwnProperty(areaName)) {
-                        Object.assign(dataByArea.ageGroup[areaName], {
-                            [dataKey]: value
-                        });
-                    } else {
-                        dataByArea.ageGroup[areaName] = {
-                            [dataKey]: value
-                        };
-                    }
-                } else {
-                    // if (dataByArea.ageGroupGender.hasOwnProperty(areaName)) {
-                    //     Object.assign(dataByArea.ageGroupGender[areaName][genderKey], {
-                    //         [dataKey]: value
-                    //     });
-                    // } else {
-                    //     dataByArea.ageGroupGender[areaName][genderKey] = {
-                    //         [dataKey]: value
-                    //     };
-                    // }
-                }
-            }
-        }
-
-        // let genderKey = rowObj.columns[1]['key'];
-        // for (let ageGroupObj of rowObj.columns[1].columns) {
-        //     let dataKey = ageGroupObj['key'];
-        //     let value = UTIL.convertToNumber(ageGroupObj['value']);
-
-        //     if (dataByArea.ageGroup.hasOwnProperty(areaName)) {
-        //         Object.assign(dataByArea.ageGroup[genderKey][areaName], {
-        //             [dataKey]: value
-        //         });
-        //     } else {
-        //         dataByArea.ageGroupp[genderKey][areaName] = {
-        //             [dataKey]: value
-        //         };
-        //     }
-        // }
-    }
-
-    let ethnicGroupByArea = rawData.ethnicGroup.filter(d => d['rowText'].toLowerCase().includes('total'));
-    for (let rowObj of ethnicGroupByArea) {
-        let areaName = UTIL.convertToTitleCase(rowObj['rowText'].toLowerCase().replace('- total', '').trim());
-
-        for (let ethnicObj of rowObj.columns) {
-            let dataKey = ethnicObj['key'];
-            let value = UTIL.convertToNumber(ethnicObj.columns[0]['value']);
-
-            if (areaName.toLowerCase() !== 'total' && dataKey.toLowerCase() == 'total' && value > dataByArea.highestPopulationCount) {
-                dataByArea.highestPopulationCount = value;
-            }
-
-            if (dataByArea.ethnicGroup.hasOwnProperty(areaName)) {
-                Object.assign(dataByArea.ethnicGroup[areaName], {
-                    [dataKey]: value
-                });
-            } else {
-                dataByArea.ethnicGroup[areaName] = {
-                    [dataKey]: value
-                };
-            }
-        }
-    }
-
-    for (let rowObj of rawData.grossIncome) {
-        let areaName = rowObj['rowText'];
-
-        for (let incomeObj of rowObj.columns) {
-            let dataKey = incomeObj['key'];
-            let value = incomeObj['value'];
-            value = UTIL.convertToNumber(value);
-
-            if (dataByArea.grossIncome.hasOwnProperty(areaName)) {
-                Object.assign(dataByArea.grossIncome[areaName], {
-                    [dataKey]: value
-                });
-            } else {
-                dataByArea.grossIncome[areaName] = {
-                    [dataKey]: value
-                };
-            }
-        }
-    }
-
-    for (let rowObj of rawData.literacy) {
-        let areaName = rowObj['rowText'];
-
-        for (let litObj of rowObj.columns) {
-            let dataKey = litObj['key'];
-            let value = litObj['value'];
-            value = UTIL.convertToNumber(value);
-
-            if (dataByArea.literacy.hasOwnProperty(areaName)) {
-                Object.assign(dataByArea.literacy[areaName], {
-                    [dataKey]: value
-                });
-            } else {
-                dataByArea.literacy[areaName] = {
-                    [dataKey]: value
-                };
-            }
-        }
-    }
-
-    for (let rowObj of rawData.occupation) {
-        let areaName = rowObj['rowText'];
-
-        for (let ocptObj of rowObj.columns) {
-            let dataKey = ocptObj['key'];
-            let value = ocptObj['value'];
-            value = UTIL.convertToNumber(value);
-
-            if (dataByArea.occupation.hasOwnProperty(areaName)) {
-                Object.assign(dataByArea.occupation[areaName], {
-                    [dataKey]: value
-                });
-            } else {
-                dataByArea.occupation[areaName] = {
-                    [dataKey]: value
-                };
-            }
-        }
-    }
-
-    for (let rowObj of rawData.qualification) {
-        let areaName = rowObj['rowText'];
-
-        for (let edcLvlObj of rowObj.columns) {
-            let dataKey = edcLvlObj['key'];
-            let value = edcLvlObj['value'];
-            value = UTIL.convertToNumber(value);
 
             if (dataByArea.qualification.hasOwnProperty(areaName)) {
                 Object.assign(dataByArea.qualification[areaName], {
@@ -229,24 +85,27 @@ function transformGeoDistributionData(rawData) {
     for (let rowObj of rawData.transportMode) {
         let areaName = rowObj['rowText'];
 
-        for (let modeObj of rowObj.columns) {
-            let dataKey = modeObj['key'];
-            let value = modeObj['value'];
-            value = UTIL.convertToNumber(value);
+    dataByArea.genderPopulation = getObjModelForGeoDistribution(ageGroupByArea, false);
+    dataByArea.ageGroup = getObjModelForGeoDistribution(ageGroupByArea, true);
+    dataByArea.ethnicGroup = getObjModelForGeoDistribution(rawData.ethnicGroup
+        .filter(d => d['rowText'].toLowerCase().includes('total')), false);
+    dataByArea.dwellingType = getObjModelForGeoDistribution(rawData.dwellingType, false);
+    dataByArea.grossIncome = getObjModelForGeoDistribution(rawData.grossIncome, false);
+    dataByArea.literacy = getObjModelForGeoDistribution(rawData.literacy, false);
+    dataByArea.occupation = getObjModelForGeoDistribution(rawData.occupation);
+    dataByArea.qualification = getObjModelForGeoDistribution(rawData.qualification, false);
+    dataByArea.transportMode = getObjModelForGeoDistribution(rawData.transportMode, false);
 
-            if (dataByArea.transportMode.hasOwnProperty(areaName)) {
-                Object.assign(dataByArea.transportMode[areaName], {
-                    [dataKey]: value
-                });
-            } else {
-                dataByArea.transportMode[areaName] = {
-                    [dataKey]: value
-                };
+    for (let areaData in dataByArea.genderPopulation) {
+        if (areaData !== 'Total') {
+            let areaPop = dataByArea.genderPopulation[areaData]['Total'];
+            if (areaPop > dataByArea.highestPopulationCount) {
+                dataByArea.highestPopulationCount = areaPop;
             }
         }
     }
 
-    console.log("Geographical Distribution:\n", dataByArea);
+    // console.log("Geographical Distribution:\n", dataByArea);
     return dataByArea;
 }
 
@@ -277,26 +136,19 @@ async function getAnnualPopulationData() {
 
 function transformAnnualPopulationData(rawData) {
     let dataByYear = {};
-    for (let rowObj of rawData['indicators']) {
+
+    for (let rowObj of rawData.indicators) {
         let dataKey = UTIL.convertDOSKeys(rowObj['rowText']);
 
         for (let colObj of rowObj.columns) {
             let year = colObj['key'];
-            if (dataByYear.hasOwnProperty(year)) {
-                dataByYear[year] = Object.assign(dataByYear[year], {
-                    [dataKey]: UTIL.convertToNumber(colObj['value'])
-                });
-            } else {
-                dataByYear[year] = {
-                    [dataKey]: UTIL.convertToNumber(colObj['value'])
-                };
-            }
+            dataByYear[year] = {...dataByYear[year], [dataKey]: UTIL.convertToNumber(colObj['value']) };
         }
     }
 
     let prevSeriesNo = null;
     let prevDataKey = null;
-    for (let rowObj of rawData['categories']) {
+    for (let rowObj of rawData.categories) {
         let dataKey = UTIL.convertDOSKeys(rowObj['rowText']);
         let seriesNo = rowObj['SeriesNo'];
 
@@ -305,26 +157,9 @@ function transformAnnualPopulationData(rawData) {
 
             if (seriesNo.startsWith(prevSeriesNo + '.')) {
                 let breakDownKey = prevDataKey + '_age_breakdown';
-
-                if (dataByYear[year].hasOwnProperty(breakDownKey)) {
-                    dataByYear[year][breakDownKey] = Object.assign(dataByYear[year][breakDownKey], {
-                        [dataKey]: UTIL.convertToNumber(colObj['Value'])
-                    });
-                } else {
-                    dataByYear[year][breakDownKey] = {
-                        [dataKey]: UTIL.convertToNumber(colObj['Value'])
-                    };
-                }
+                dataByYear[year][breakDownKey] = {...dataByYear[year][breakDownKey], [dataKey]: UTIL.convertToNumber(colObj['Value']) };
             } else {
-                if (dataByYear.hasOwnProperty(year)) {
-                    dataByYear[year] = Object.assign(dataByYear[year], {
-                        [dataKey]: UTIL.convertToNumber(colObj['Value'])
-                    });
-                } else {
-                    dataByYear[year] = {
-                        [dataKey]: UTIL.convertToNumber(colObj['Value'])
-                    };
-                }
+                dataByYear[year] = {...dataByYear[year], [dataKey]: UTIL.convertToNumber(colObj['Value']) };
             }
         }
 
